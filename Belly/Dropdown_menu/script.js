beabe14751c41a66104bd6b5159b029493a8f82b
Kellen <kellen.finn@gmail.com>
2022-12-21 15:34:13 -0500

    updated with plotly trials and dynamic dashbaord from JSON bio samples to display full detail in panel

--- Belly/Dropdown_menu/script.js
+++ Belly/Dropdown_menu/script.js
@@ -1,13 +1,3 @@
-d3.json("samples.json").then(function(data){
-    console.log(data);
-});
-
-d3.json("samples.json").then(function(data){
-    firstPerson = data.metadata[0];
-    Object.entries(firstPerson).forEach(([key, value]) =>
-      {console.log(key + ': ' + value);});
-});
-
 d3.selectAll("body").on("change", updatePage);
 
 function updatePage() {
